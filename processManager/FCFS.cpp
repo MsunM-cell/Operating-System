096a6e883e771547f01c1951ce8a4b2490d4c019
lukc12138 <102399888+lukc12138@users.noreply.github.com>
2022-03-26 15:42:49 +0800

    V1

--- processManager/FCFS.cpp
+++ processManager/FCFS.cpp
@@ -1,6 +1,41 @@
+/*
+ * @Author: LuChang
+ * @Date: 2022-03-26 13:29:50
+ * @LastEditors: LuChang
+ * @LastEditTime: 2022-03-26 15:41:18
+ * @FilePath: \osDesign\OS\Operating-System\processManager\FCFS.cpp
+ * @Description: 
+ *      processmanager,using FCFS algorithm
+ * Copyright (c) 2022 by LuChang, All Rights Reserved. 
+ */
+
 #include <iostream>
-using namespace std;
-int main(){
-    cout << "testGit" << endl;
-    return 0;
+#include "proc.h"
+class procManagerFCFS{
+    public:
+        void addToQueue(PCB p);
+        void runProcManager();
+        void run(PCB p);
+    private:
+        queue <PCB> fcfsQueue;
+    
+};
+
+void procManagerFCFS::addToQueue(PCB p){
+    fcfsQueue.push(p);
+}
+
+void procManagerFCFS::runProcManager(){
+    while(true){
+        while(!fcfsQueue.empty()){
+            PCB p = fcfsQueue.front();
+            //该函数是执行函数，暂时未定
+            run(p);
+            fcfsQueue.pop();
+        }
+    }
+}
+
+void procManagerFCFS::run(PCB p){
+    std::cout << "process " << p.id << " is running" << endl;
 }
---
+++ processManager/proc.h
@@ -0,0 +1,41 @@
+#ifndef PROC_H
+#define PROC_H
+
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <queue>
+#include <vector>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+
+// 宏
+#define WIDETH 10
+#define TIME_SLICE 200
+
+// PCB的结构体
+typedef struct 
+{
+   // pid
+   int id;
+   // 优先级
+   int pri; 
+   // 预计还需要的时间
+   int time_need;
+} PCB;
+
+// 全局变量
+// rr队列
+static queue<PCB*> rr_que;
+// 保存目前活跃的PCB
+static vector<PCB*> active_pcb;
+// 程序运行的开始时间
+clock_t system_start;
+// 时间片开始的时间
+static clock_t slice_start;
+
+static queue <PCB> FCFS;
+
+#endif // !PROC_H
+
