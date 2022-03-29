07b28d852a4a12efd16c6bfbf3a79233dc1e8428
kahakaha <793779810@qq.com>
2022-03-29 11:16:11 +0800

    封装了RR队列，功能不完善

--- processManager/RR.cpp
+++ processManager/RR.cpp
@@ -7,14 +7,17 @@
 #include "proc.h"
 using namespace std;
 
-extern queue<PCB*> rr_que;
-extern vector<PCB*> active_pcb;
-extern clock_t system_start, slice_start;
 
-void init()
+RRQueue::RRQueue()
 {
-    srand(time(NULL));
-    for (int i = 0; i < 5; i++)
+    this->size = 0;
+}
+
+RRQueue::RRQueue(int n_size, int x_size)
+{
+    this->size = n_size + x_size;
+    // 不会被降级的PCB
+    for (int i = 0; i < n_size; i++)
     {
         PCB* new_pcb = (PCB*) malloc(sizeof(PCB));
         new_pcb->id = i;
@@ -22,35 +25,46 @@ void init()
         new_pcb->slice_cnt = 0;
         new_pcb->time_need = rand() % 30 * 100;
         active_pcb.push_back(new_pcb);
-        rr_que.push(new_pcb);
+        this->rr_que.push(new_pcb);
     }
     // 降级测试
-    PCB* new_pcb = (PCB*) malloc(sizeof(PCB));
-    new_pcb->id = 5;
-    new_pcb->pri = HIGH_PRI;
-    new_pcb->slice_cnt = 0;
-    new_pcb->time_need = 5000;
-    active_pcb.push_back(new_pcb);
-    rr_que.push(new_pcb);
-
+    for (int i = 0; i < x_size; i++)
+    {
+        PCB* new_pcb = (PCB*) malloc(sizeof(PCB));
+        new_pcb->id = n_size + i;
+        new_pcb->pri = HIGH_PRI;
+        new_pcb->slice_cnt = 0;
+        new_pcb->time_need = rand() % 10 * 100 + TIME_SLICE * MAX_CNT;
+        active_pcb.push_back(new_pcb);
+        this->rr_que.push(new_pcb);
+    }
 }
 
-int main(void)
+RRQueue::~RRQueue()
 {
-    init();
+    cout << "RR queue takes a nap.\n";
+}
+
+int RRQueue::getSize()
+{
+    return this->size;
+}
+
+int main()
+{
+    RRQueue test_queue(5,2);
     // 系统开始计时，实际应该更早
     system_start = clock();
     printf("[%ld]This is a RR test\n", clock() - system_start);
     cout << setiosflags(ios::left);
-    cout << setw(WIDETH) << "Id" << setw(WIDETH) << "Time_need\n";
+    cout << setw(WIDTH) << "Id" << setw(WIDTH) << "Time_need\n";
 
     // 循环到rr队列为空
-    while (rr_que.empty() != true)
+    while (test_queue.getSize() != true)
     {
         PCB* cur_pcb =  rr_que.front();
         rr_que.pop();
-        cout << setw(WIDETH) << cur_pcb->id << setw(WIDETH) << cur_pcb->time_need << endl;
-        slice_start = clock();
+        cout << setw(WIDTH) << cur_pcb->id << setw(WIDTH) << cur_pcb->time_need << endl;
         // 判断时间是否够完成一次循环
         if (cur_pcb->time_need > TIME_SLICE)
         {
@@ -59,7 +73,7 @@ int main(void)
             printf("[%ld]Pid %d time out! Still need %d.\n", clock() - system_start, cur_pcb->id, cur_pcb->time_need);
             // 判断一下是否使用了过多的时间片,是则降级
             cur_pcb->slice_cnt++;
-            if (cur_pcb->slice_cnt == 10)
+            if (cur_pcb->slice_cnt == MAX_CNT)
             {
                 cur_pcb->pri = LOW_PRI;
                 // 降级加入fcfs队列中
--- processManager/proc.h
+++ processManager/proc.h
@@ -12,14 +12,14 @@ using namespace std;
 
 // 宏
 // 对齐宽度
-#define WIDETH 10
+#define WIDTH 10
 // 时间片大小
 #define TIME_SLICE 200
 // 高优先级
 #define HIGH_PRI 0
 // 低优先级
 #define LOW_PRI 1
-// 最大时间片数量
+// 最大占用的时间片数量
 #define MAX_CNT 10
 
 // PCB的结构体
@@ -28,7 +28,9 @@ typedef struct
    // pid
    int id;
    // 优先级
-   int pri; 
+   int pri;
+   // 使用过的时间片数量
+   int slice_cnt;
    // 预计还需要的时间
    int time_need;
 } PCB;
@@ -40,15 +42,45 @@ bool IO[2];
 
 // 全局变量
 // rr队列
-static queue<PCB*> rr_que;
+// static queue<PCB*> rr_que;
 // 保存目前活跃的PCB
 static vector<PCB*> active_pcb;
 // 程序运行的开始时间
 clock_t system_start;
-// 时间片开始的时间
-static clock_t slice_start;
 
 static queue <PCB> FCFS;
+
+/**
+ * @brief RR队列类
+ */
+class RRQueue {
+private:
+    //目前队列中PCB的数量
+    int size;
+    //rr队列本身
+    queue<PCB*> rr_que;
+public:
+    /**
+     * @brief RR队列的一般构造
+     * @param void
+     */
+    RRQueue();
+    /**
+     * @brief 仅会在测试中使用，根据输入的大小随机构造
+     * @param n_size 队列中不会被降级的个数
+     * @param x_size 被降级的个数
+     */
+    RRQueue(int n_size, int x_size);
+    /**
+     * @brief 析构函数
+     */
+    ~RRQueue();
+    /**
+     * @brief 查询目前队列的长度
+     * @return 队列长度
+     */
+    int getSize();
+};
 
 #endif // !PROC_H
 
