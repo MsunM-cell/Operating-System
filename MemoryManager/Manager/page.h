26cceca89f416262efd280d7c8d2a527f4e9c351
JJJackyL <2878442591@qq.com>
2022-05-20 08:32:43 +0800

    update

--- MemoryManager/Manager/page.h
+++ MemoryManager/Manager/page.h
@@ -26,7 +26,7 @@ private:
 public:
     //构造函数和析构函数
     BasicPageManager() { init_manager(); }
-    ~BasicPageManager(){ printf("Exit BasicPageManager\n"); }
+    ~BasicPageManager(){}
     //访问内存接口
     char accessMemory(int pid, int address);
     //写内存接口
