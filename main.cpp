74b01982957fab4e0d045889c84d3848050316be
“2019211532 <1285829101@qq.com>
2022-05-24 13:09:42 +0800

    ..

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 // 输入缓冲区
-string inBuf="";
+string inBuf = "";
 // 互斥锁
 HANDLE hMutex = NULL;
 // 信号量
@@ -22,14 +22,14 @@ HANDLE hSemaphore = INVALID_HANDLE_VALUE;
 
 /**
  * @brief 一个获取用户输入的函数
- * 
+ *
  */
 DWORD WINAPI getch(LPVOID lpParamter)
 {
     while (inBuf != "exit")
     {
-        WaitForSingleObject(hMutex,INFINITE);
-        getline(cin,inBuf);
+        WaitForSingleObject(hMutex, INFINITE);
+        getline(cin, inBuf);
         ReleaseMutex(hMutex);
         ReleaseSemaphore(hSemaphore, 1, NULL);
     }
@@ -37,13 +37,13 @@ DWORD WINAPI getch(LPVOID lpParamter)
 }
 /**
  * @brief 进程管理器调度的线程函数，确保可以边接收指令边调度
- * 
- * @param lpParamter 
- * @return DWORD 
+ *
+ * @param lpParamter
+ * @return DWORD
  */
 DWORD WINAPI setupProcManager(LPVOID lpParamter)
 {
-    while(true)
+    while (true)
     {
         ProcManager::getInstance().scheduling();
         if (ProcManager::getInstance().getActiveNum() == 0)
@@ -58,7 +58,7 @@ DWORD WINAPI setupProcManager(LPVOID lpParamter)
 
 /**
  * @brief 对用户指令进行解释
- * 
+ *
  * @param cmd 输入指令
  * @param argv 输出的参数数量
  * @return int 参数数量
@@ -73,18 +73,18 @@ int parse(string cmd, vector<string> &argv)
     {
         argv.push_back(token);
     }
-    return argv.size(); 
+    return argv.size();
 }
 
 /**
  * @brief 新建pcb
- * 
+ *
  * @param file 传入的文件
  */
-PCB* createPCB(json file,string path)
+PCB *createPCB(json file, string path)
 {
-    PCB* ptr = new PCB;
-    ptr->id= ProcManager::getInstance().getAvailableId();
+    PCB *ptr = new PCB;
+    ptr->id = ProcManager::getInstance().getAvailableId();
     ptr->path = path;
     ptr->name = file["name"];
     ptr->status = NEW;
@@ -94,12 +94,11 @@ PCB* createPCB(json file,string path)
     ptr->slice_cnt = 0;
     ptr->time_need = 5000;
     ptr->cpu_time = 0;
-    
+
     return ptr;
 }
 
-
-int main(int argc1, char* argv1[])
+int main(int argc1, char *argv1[])
 {
     ofstream pout("./processManager/debug.txt");
     if (argc1 > 1)
@@ -108,6 +107,7 @@ int main(int argc1, char* argv1[])
         {
             pbug.rdbuf(cout.rdbuf());
             pout.close();
+            // Log::setLogMode(Log::INFO);
         }
     }
     else
@@ -126,8 +126,8 @@ int main(int argc1, char* argv1[])
     // 进程调度线程
     HANDLE procManagerThread = CreateThread(NULL, 0, setupProcManager, NULL, 0, NULL);
     CloseHandle(procManagerThread);
-    string cmd="";
-    int args=0;
+    string cmd = "";
+    int args = 0;
     vector<string> argv;
 
     // 文件管理初始化
@@ -140,20 +140,22 @@ int main(int argc1, char* argv1[])
         pwd = usrname + "@My-OS:" + fm.working_path + "$ ";
         cout << pwd;
         WaitForSingleObject(hSemaphore, INFINITE);
-        WaitForSingleObject(hMutex,INFINITE);
+        WaitForSingleObject(hMutex, INFINITE);
         cmd = inBuf;
         // cout << cmd << endl;
         args = parse(cmd, argv);
         // cout << argv[0];
         // 根据分析出的指令执行相关的操作
-        if(args == 0){
+        if (args == 0)
+        {
             // puts("error");
             ReleaseMutex(hMutex);
             // system("pause");
             continue;
             // break;
         }
-        if (argv[0] == "ps") {
+        if (argv[0] == "ps")
+        {
             if (args == 1)
             {
                 ProcManager::getInstance().ps();
@@ -178,7 +180,8 @@ int main(int argc1, char* argv1[])
                 cout << "unknown cmd!\n";
             }
         }
-        else if(argv[0] == "cm"){
+        else if (argv[0] == "cm")
+        {
             if (bmm->getMode() == "block")
             {
                 bmm->compress_mem();
@@ -192,45 +195,51 @@ int main(int argc1, char* argv1[])
             {
                 string path = fileOperation.pathConverter(argv[1]);
                 string get_file_path = path.substr(fm.home_path.size());
-                if (path == "error" || !exists(path)) {
+                if (path == "error" || !exists(path))
+                {
                     puts("run: File path wrong");
                 }
-                else if (is_directory(path)) {
+                else if (is_directory(path))
+                {
                     printf("'%s' is a directory.\n", argv[1].c_str());
                 }
-                else {
+                else
+                {
                     // cout << path << endl;
-                    PCB* pcb;
+                    PCB *pcb;
                     json file;
                     file = fm.get_file(get_file_path, "read", "FCFS");
                     // cout << file << endl;
-                    if (string(file["type"])[0] != 'e') {
+                    if (string(file["type"])[0] != 'e')
+                    {
                         printf("'%s' is not an executable file.\n", argv[1].c_str());
                     }
-                    else if (string(file["type"])[3] != 'x') {
+                    else if (string(file["type"])[3] != 'x')
+                    {
                         printf("'%s' Permission denied.\n", argv[1].c_str());
                     }
-                    else {
-                        pcb = createPCB(file,path);
+                    else
+                    {
+                        pcb = createPCB(file, path);
                         ProcManager::getInstance().run(pcb);
                     }
-                    
                 }
             }
             else
             {
                 cout << "unknown cmd!\n";
             }
-
         }
-        else if(argv[0] == "ls"){
-            if(args == 2){
+        else if (argv[0] == "ls")
+        {
+            if (args == 2)
+            {
                 fileOperation.ls_command(argv[1]);
             }
-            else{
+            else
+            {
                 fileOperation.ls_command("");
             }
-            
         }
         else if (argv[0] == "td")
         {
@@ -240,11 +249,12 @@ int main(int argc1, char* argv1[])
         {
             fm.display_storage_status();
         }
-        else if(argv[0]=="dms")
+        else if (argv[0] == "dms")
         {
             bmm->dms_command();
         }
-        else if(argv[0] == "cd"){
+        else if (argv[0] == "cd")
+        {
             fileOperation.cd_command(argv[1]);
         }
         else if (argv[0] == "exit" || argv[0] == "yes")
@@ -257,78 +267,102 @@ int main(int argc1, char* argv1[])
             string path = fm.home_path + fm.working_path + "cpu2";
             string get_file_path = fm.working_path + "cpu2";
             // cout << path << endl;
-            PCB* pcb;
+            PCB *pcb;
             file = fm.get_file(get_file_path, "read", "FCFS");
             for (int i = 0; i < 5; i++)
             {
-                pcb = createPCB(file,path);
+                pcb = createPCB(file, path);
                 ProcManager::getInstance().run(pcb);
-            } 
-        }  
-        else if(argv[0] == "rm"){
-            if(args == 2 && argv[1] != "-r"){
+            }
+        }
+        else if (argv[0] == "rm")
+        {
+            if (args == 2 && argv[1] != "-r")
+            {
                 string dir = "";
                 string file_name = "";
-                if (argv[1].front() == path::preferred_separator) {
+                if (argv[1].front() == path::preferred_separator)
+                {
                     dir = argv[1].substr(0, argv[1].find_last_of(path::preferred_separator));
                     file_name = argv[1].substr(argv[1].find_last_of(path::preferred_separator) + 1);
-                } else {
-                    if (argv[1].find(path::preferred_separator) == string::npos) {
+                }
+                else
+                {
+                    if (argv[1].find(path::preferred_separator) == string::npos)
+                    {
                         dir = ".";
                         file_name = argv[1];
                     }
-                    else {
+                    else
+                    {
                         dir = argv[1].substr(0, argv[1].find_last_of(path::preferred_separator));
                         file_name = argv[1].substr(argv[1].find_last_of(path::preferred_separator) + 1);
                     }
                 }
                 fileOperation.delete_file(dir, file_name);
             }
-            else if (args == 3) {
+            else if (args == 3)
+            {
                 bool recursive = false;
                 int index = -1;
 
-                for (int i = 0; i < 3; i++) 
-                    if (argv[i] == "-r") {
+                for (int i = 0; i < 3; i++)
+                    if (argv[i] == "-r")
+                    {
                         recursive = true;
                         index = i;
                         break;
                     }
-                
-                if (!recursive) {
+
+                if (!recursive)
+                {
                     puts("error");
-                } else {
-                    
-                    if (index == 1) {
+                }
+                else
+                {
+
+                    if (index == 1)
+                    {
                         string dir = "";
                         string file_name = "";
-                        if (argv[2].front() == path::preferred_separator) {
+                        if (argv[2].front() == path::preferred_separator)
+                        {
                             dir = argv[2].substr(0, argv[2].find_last_of(path::preferred_separator));
                             file_name = argv[2].substr(argv[2].find_last_of(path::preferred_separator) + 1);
-                        } else {
-                            if (argv[2].find(path::preferred_separator) == string::npos) {
+                        }
+                        else
+                        {
+                            if (argv[2].find(path::preferred_separator) == string::npos)
+                            {
                                 dir = ".";
                                 file_name = argv[2];
                             }
-                            else {
+                            else
+                            {
                                 dir = argv[2].substr(0, argv[2].find_last_of(path::preferred_separator));
                                 file_name = argv[2].substr(argv[2].find_last_of(path::preferred_separator) + 1);
                             }
                         }
                         fileOperation.delete_dir(dir, file_name);
                     }
-                    else {
+                    else
+                    {
                         string dir = "";
                         string file_name = "";
-                        if (argv[1].front() == path::preferred_separator) {
+                        if (argv[1].front() == path::preferred_separator)
+                        {
                             dir = argv[1].substr(0, argv[1].find_last_of(path::preferred_separator));
                             file_name = argv[1].substr(argv[1].find_last_of(path::preferred_separator) + 1);
-                        } else {
-                            if (argv[1].find(path::preferred_separator) == string::npos) {
+                        }
+                        else
+                        {
+                            if (argv[1].find(path::preferred_separator) == string::npos)
+                            {
                                 dir = ".";
                                 file_name = argv[1];
                             }
-                            else {
+                            else
+                            {
                                 dir = argv[1].substr(0, argv[1].find_last_of(path::preferred_separator));
                                 file_name = argv[1].substr(argv[1].find_last_of(path::preferred_separator) + 1);
                             }
@@ -336,79 +370,104 @@ int main(int argc1, char* argv1[])
                         fileOperation.delete_dir(dir, file_name);
                     }
                 }
-
             }
-            else{
+            else
+            {
                 puts("error");
             }
         }
-        else if(argv[0] == "mkdir"){
-            if(args == 2){
+        else if (argv[0] == "mkdir")
+        {
+            if (args == 2)
+            {
                 string dir = "";
                 string dir_name = "";
-                if (argv[1].front() == path::preferred_separator) {
+                if (argv[1].front() == path::preferred_separator)
+                {
                     dir = argv[1].substr(0, argv[1].find_last_of(path::preferred_separator));
                     dir_name = argv[1].substr(argv[1].find_last_of(path::preferred_separator) + 1);
-                } else {
-                    if (argv[1].find(path::preferred_separator) == string::npos) {
+                }
+                else
+                {
+                    if (argv[1].find(path::preferred_separator) == string::npos)
+                    {
                         dir = ".";
                         dir_name = argv[1];
                     }
-                    else {
+                    else
+                    {
                         dir = argv[1].substr(0, argv[1].find_last_of(path::preferred_separator));
                         dir_name = argv[1].substr(argv[1].find_last_of(path::preferred_separator) + 1);
                     }
                 }
                 fileOperation.create_dir(dir, dir_name);
             }
-            else{
+            else
+            {
                 puts("error");
             }
         }
-        else if(argv[0] == "touch"){
-            if(args == 2){
+        else if (argv[0] == "touch")
+        {
+            if (args == 2)
+            {
                 string dir = "";
                 string file_name = "";
-                if (argv[1].front() == path::preferred_separator) {
+                if (argv[1].front() == path::preferred_separator)
+                {
                     dir = argv[1].substr(0, argv[1].find_last_of(path::preferred_separator));
                     file_name = argv[1].substr(argv[1].find_last_of(path::preferred_separator) + 1);
-                } else {
-                    if (argv[1].find(path::preferred_separator) == string::npos) {
+                }
+                else
+                {
+                    if (argv[1].find(path::preferred_separator) == string::npos)
+                    {
                         dir = ".";
                         file_name = argv[1];
                     }
-                    else {
+                    else
+                    {
                         dir = argv[1].substr(0, argv[1].find_last_of(path::preferred_separator));
                         file_name = argv[1].substr(argv[1].find_last_of(path::preferred_separator) + 1);
                     }
                 }
                 fileOperation.create_file(dir, file_name);
             }
-            else{
+            else
+            {
                 puts("error");
             }
         }
-        else if(argv[0] == "cp"){
-            if(args == 3){
+        else if (argv[0] == "cp")
+        {
+            if (args == 3)
+            {
                 fileOperation.copy_file(argv[1], argv[2]);
             }
-            else{
+            else
+            {
                 puts("error");
             }
         }
-        else if(argv[0] == "mv"){
-            if(args == 3){
+        else if (argv[0] == "mv")
+        {
+            if (args == 3)
+            {
                 fileOperation.move_file(argv[1], argv[2]);
             }
-            else{
+            else
+            {
                 puts("error");
             }
         }
-        else if (argv[0] == "chmod") {
-            if (args == 3) {
+        else if (argv[0] == "chmod")
+        {
+            if (args == 3)
+            {
                 fileOperation.modify_file_type(argv[1], 0 + argv[2][0]);
             }
-            else {
+            else
+            {
                 puts("error");
             }
         }
