e57508e0ef504ff2077da74e735a0d459c129253
Spider <2464831035@qq.com>
2022-05-24 11:58:48 +0800

    test

--- FileManager/file_manager.cpp
+++ FileManager/file_manager.cpp
@@ -557,6 +557,7 @@ json FileManager::path2dict(string file_path)
 void FileManager::get_file_demo(string seek_algo)
 {
     vector<pair<int, int>> seek_queue;
+    cout << seek_algo << endl;
     seek_queue = {{98, 3}, {183, 5}, {37, 2}, {122, 11}, {119, 5}, {14, 0}, {124, 8}, {65, 5}, {67, 1}, {198, 5}, {105, 5}, {53, 3}};
     if (seek_algo == "FCFS")
         this->disk.FCFS(seek_queue);
@@ -957,24 +958,42 @@ json FileManager::get_file_system_tree()
     return file_system_tree;
 }
 
-// int main()
-// {
-//     FileManager fm(512, 200, 12);
-//     // fm.display_storage_status();
-//     // fm.tidy_disk();
-//     // fm.write_data("/a.txt", 120);
-//     fm.print_file_system_tree(fm.home_path);
-//     // fm.set_disk_head_pointer(110);
-//     // fm.get_file_demo("C-SCAN");
-//     // fm.set_disk_head_pointer(12);
-//     // fm.get_file_demo("SSTF");
-//     // Disk d(512, 200, 12);
-//     // vector<pair<int, int>> seek_queue;
-//     // seek_queue.push_back({100, 1});
-//     // seek_queue.push_back({40, 1});
-//     // seek_queue.push_back({60, 1});
-//     // seek_queue.push_back({10, 1});
-//     // d.SSTF(seek_queue);
-//     system("pause");
-//     return 0;
-// }
+int main()
+{
+    FileManager fm(512, 200, 12);
+    // fm.display_storage_status();
+    fm.set_disk_head_pointer(100);
+    fm.get_file_demo("FCFS");
+
+    fm.set_disk_head_pointer(100);
+    fm.get_file_demo("SSTF");
+
+    fm.set_disk_head_pointer(100);
+    fm.get_file_demo("SCAN");
+
+    fm.set_disk_head_pointer(100);
+    fm.get_file_demo("C-SCAN");
+
+    fm.set_disk_head_pointer(100);
+    fm.get_file_demo("LOOK");
+
+    fm.set_disk_head_pointer(100);
+    fm.get_file_demo("C-LOOK");
+
+    // fm.tidy_disk();
+    // fm.write_data("/a.txt", 120);
+    // fm.print_file_system_tree(fm.home_path);
+    // fm.set_disk_head_pointer(110);
+    // fm.get_file_demo("C-SCAN");
+    // fm.set_disk_head_pointer(12);
+    // fm.get_file_demo("SSTF");
+    // Disk d(512, 200, 12);
+    // vector<pair<int, int>> seek_queue;
+    // seek_queue.push_back({100, 1});
+    // seek_queue.push_back({40, 1});
+    // seek_queue.push_back({60, 1});
+    // seek_queue.push_back({10, 1});
+    // d.SSTF(seek_queue);
+    // system("pause");
+    return 0;
+}
