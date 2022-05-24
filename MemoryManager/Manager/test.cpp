7c3e0f90b0fffef2e906ca7f115e6a00befe6c90
JJJackyL <2878442591@qq.com>
2022-05-24 10:04:21 +0800

    lyx

--- MemoryManager/Manager/test.cpp
+++ MemoryManager/Manager/test.cpp
@@ -1,13 +1,12 @@
 #include "MemoryManager.cpp"
 
-
 int main()
 {
     PCB a, b, c, d;
-    a.id = 1001, a.size = 12 * 1024, a.path = "home/test"; // 12KB
-    b.id = 1002, b.size = 6 * 1024, b.path = "home/test";  // 6KB
-    c.id = 1003, c.size = 10 * 1024, c.path = "home/test"; // 10KB
-    d.id = 1004, d.size = 4 * 1024, d.path = "home/test";  // 4KB
+    a.id = 1001, a.size = 1234, a.path = "MemoryManager/Manager/test"; // 12KB
+    b.id = 1002, b.size = 2 * 1024, b.path = "MemoryManager/Manager/test";  // 6KB
+    c.id = 1003, c.size = 500, c.path = "MemoryManager/Manager/test"; // 10KB
+    d.id = 1004, d.size = 2 * 1024+1, d.path = "MemoryManager/Manager/test";  // 4KB
 
     MemoryManager *bmm = MemoryManager::getInstance();
     bmm->createProcess(a);
@@ -21,10 +20,11 @@ int main()
     bmm->createProcess(d);
     bmm->dms_command();
     bmm->compress_mem();
-    bmm->dms_command();
-    bmm->accessMemory(a.id, 200);
-    char s = 'a';
-    bmm->writeMemory(100, s, a.id);
-    bmm->accessMemory(a.id, 200);
+    //bmm->dms_command();
+    printf("pid[%d]:%c in position 200\n\n", a.id, bmm->accessMemory(a.id, 200));
+    // bmm->accessMemory(a.id, 200);
+    bmm->writeMemory(200, 'a', a.id);
+    printf("pid[%d]:%c in position 200\n\n", a.id, bmm->accessMemory(a.id, 200));
+    //bmm->accessMemory(a.id, 200);
     return 0;
 }
